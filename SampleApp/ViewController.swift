1e3aee75b48004003e4e18e9a1fc6feccd43d1ea
Ho Kam Hung <kevinho0236@gmail.com>
2019-10-02 01:44:43 +0900

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Ho Kam Hung on 2/10/2019.
+//  Copyright © 2019 Ho Kam Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
