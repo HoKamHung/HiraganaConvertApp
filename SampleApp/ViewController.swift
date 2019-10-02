6393d854b3ca9bda3c3bbd661f2db5e6f9ab408f
Ho Kam Hung <kevinho0236@gmail.com>
2019-10-02 23:09:31 +0900

    Adjust auto layout value when keyboard shows and hide.

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
+enum Device: Int
+{
+    case iPhone5
+    case iPhone6
+    case iPhone6Plus
+    case iPhoneX
+    case iPhoneXSMax
+    case iPhoneXR
+    case Unknown
+}
+
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, NetworkControllerDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var BottomBarViewBottomConstraint: NSLayoutConstraint!
     
     var items: Array<SentenceDataObject> = Array<SentenceDataObject>()
     var networkController: NetworkController = NetworkController()
@@ -18,6 +30,50 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (Notification) in
+//            <#code#>
+//        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification)
+    {
+        guard let info = notification.userInfo else {return}
+        guard let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue) else {return}
+
+        switch DeviceType(){
+            case .iPhone6:
+                    BottomBarViewBottomConstraint.constant = keyboardSize.cgRectValue.height - 16
+            case .iPhone6Plus:
+                    BottomBarViewBottomConstraint.constant = keyboardSize.cgRectValue.height - 16
+            case .iPhoneX:
+                    BottomBarViewBottomConstraint.constant = keyboardSize.cgRectValue.height - 50
+            case .iPhoneXR:
+                    BottomBarViewBottomConstraint.constant = keyboardSize.cgRectValue.height - 50
+            case .iPhoneXSMax:
+                BottomBarViewBottomConstraint.constant = keyboardSize.cgRectValue.height - 50
+            case .iPhone5:
+                BottomBarViewBottomConstraint.constant = keyboardSize.cgRectValue.height - 16
+            case .Unknown:
+                BottomBarViewBottomConstraint.constant = keyboardSize.cgRectValue.height;
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification)
+    {
+        BottomBarViewBottomConstraint.constant = 0;
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -35,9 +91,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "SentenceItem", for: indexPath) as? SentenceTableViewCell else {
                 return UITableViewCell()
             }
-    //        cell.foodImage.image = UIImage(named: food[indexPath.row].image)
-    //        cell.foodNameLabel.text = food[indexPath.row].name
-    //        cell.addressLabel.text = food[indexPath.row].address
             let item: SentenceDataObject? = items[indexPath.row]
             if item != nil {
                 cell.InputLabel.text = item?.inputString
@@ -86,12 +139,34 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func responseStringOfHiragana(_ hiraganaString: String, index: Int) {
-        var dataObject: SentenceDataObject = items[index]
+        let dataObject: SentenceDataObject = items[index]
         dataObject.outputString = hiraganaString
         
         DispatchQueue.main.async {
             self.tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: UITableView.RowAnimation.fade)
         }
+    }
+    
+    func DeviceType()-> Device{
+        if UIDevice().userInterfaceIdiom == .phone {
+            switch UIScreen.main.nativeBounds.height {
+            case 1136:
+                return Device.iPhone5
+            case 1334:
+                return Device.iPhone6
+            case 1920, 2208:
+                return Device.iPhone6Plus
+            case 2436:
+                return Device.iPhoneX
+            case 2688:
+                return Device.iPhoneXSMax
+            case 1792:
+                return Device.iPhoneXR
+            default:
+                return Device.Unknown
+            }
+        }
+        return Device.Unknown
     }
 }
 
